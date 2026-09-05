395f8d4c903bc1384d2656522a8705f4813a03cb
jerryjerry0622797-boop <jerryjerry0622797@gmail.com>
2026-09-05 04:58:39 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Punjab Board Result 2026',
+  description: 'Check 11th and 12th Class Result for all 9 Punjab BISE Boards',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
