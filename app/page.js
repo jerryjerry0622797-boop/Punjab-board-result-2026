a4ae2675877aeb9af9a90a9fd25fc43d0286b2d6
jerryjerry0622797-boop <jerryjerry0622797@gmail.com>
2026-09-05 05:34:14 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client"
+export const dynamic = 'force-dynamic'
 export const metadata = {
   title: "Punjab Board 11th 12th Result 2026 | All BISE Boards Check Online",
   description: "Check 11th and 12th Class Result 2026 for all Punjab Boards: Lahore, Gujranwala, Faisalabad, Multan, Rawalpindi. Result Date: 23 September 2026.",
