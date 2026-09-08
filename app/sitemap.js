249cf56bd370d3ee213030336a1fcf285e7eb094
jerryjerry0622797-boop <jerryjerry0622797@gmail.com>
2026-09-07 19:32:00 -0700

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://punjab-board-result-2026-iota.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
